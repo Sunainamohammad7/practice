486b3aa8724d67287e7461305ba39d1720d288ed
Sunaina <mohammadsunaina9@gmail.com>
2026-03-17 12:09:46 +0530

    Added a line

--- practice.py
+++ practice.py
@@ -1,3 +1,6 @@
 l=[1,2,3,4,5,6,7,8]
-a=[i*2 for i in l] 
+a=[i*2 for i in l]
+b=[i*2 for i in l if i%2==0]
+c=[i for i in l if i%2!=0]
 print("This are the numbers that are multiplied by two:", a)
+print("This are the numbers that are multiples of two:", b)
