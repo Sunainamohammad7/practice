9396148c37ed9e5a581f5eb461b1ea06788665b6
Sunaina <mohammadsunaina9@gmail.com>
2026-03-17 12:12:18 +0530

    added other line

--- practice.py
+++ practice.py
@@ -4,3 +4,4 @@ b=[i*2 for i in l if i%2==0]
 c=[i for i in l if i%2!=0]
 print("This are the numbers that are multiplied by two:", a)
 print("This are the numbers that are multiples of two:", b)
+print("This are the numbers that are odd numbers:",c)
